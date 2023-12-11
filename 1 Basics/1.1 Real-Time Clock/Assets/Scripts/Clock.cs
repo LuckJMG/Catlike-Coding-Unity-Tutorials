e8dad1456faf82ed8b049032d4097a0009a00ba8
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-12-11 11:57:05 -0300

    1.1 Real-Time Clock

---
+++ 1 Basics/1.1 Real-Time Clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+	[SerializeField] Transform hoursPivot, minutesPivot, secondsPivot;
+
+	const float hoursToDegrees = -30f;
+	const float minutesToDegrees = -6f;
+	const float secondsToDegrees = -6f;
+
+	void Update() {
+		TimeSpan time = DateTime.Now.TimeOfDay;
+		hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+		minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+		secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+	}
+}
