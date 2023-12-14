99ccaaabde73d6a532ebbec4ae1797187775e581
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-12-14 11:45:40 -0300

    1.3 3D graphs

--- 1 Basics/1.2 Animated Graph/Assets/Scripts/Graph.cs
+++ 1 Basics/1.2 Animated Graph/Assets/Scripts/Graph.cs
@@ -3,34 +3,38 @@ using UnityEngine;
 public class Graph : MonoBehaviour {
 	[SerializeField] Transform pointPrefab;
 	[SerializeField, Range(10, 100)] int resolution = 10;
+	[SerializeField] FunctionLibrary.FunctionName function;
 
 	Transform[] points;
 
 	void Awake() {
-		points = new Transform[resolution];
+		points = new Transform[resolution * resolution];
 
 		float step = 2f / resolution;
-		var position = Vector3.zero;
 		var scale = Vector3.one * step;
 
 		for (int i = 0; i < points.Length;  i++) {
-			position.x = (i + 0.5f) * step - 1f;
-
 			Transform point = points[i] = Instantiate(pointPrefab);
-			point.SetParent(transform, false);
-			point.localPosition = position;
 			point.localScale = scale;
+			point.SetParent(transform, false);
 		}
 	}
 
 	void Update() {
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
 		float time = Time.time;
+		float step = 2f / resolution;
+		float v = 0.5f * step - 1f;
 
-		for (int i = 0; i < points.Length; i++) {
-			Transform point = points[i];
-			Vector3 position = point.localPosition;
-			position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-			point.localPosition = position;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+			if (x == resolution) {
+				x = 0;
+				z += 1;
+				v = (z + 0.5f) * step - 1f;
+			}
+
+			float u = (x + 0.5f) * step - 1f;
+			points[i].localPosition = f(u, v, time);
 		}
 	}
 }
